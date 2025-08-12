7fedae74f5c25d6b891a48ec5559a13d2f61db1a
Jose Tovar <jtovar26455462@gmail.com>
2025-08-12 17:15:00 -0400

    feat: add handle gig adjustment price backend

---
+++ app/actions/stripe/handle-gig-adjustment.ts
@@ -0,0 +1,80 @@
+import { eq } from 'drizzle-orm';
+import { db } from '@/lib/drizzle/db';
+import { GigsTable, UsersTable } from '@/lib/drizzle/schema';
+import { getPaymentAccountDetailsForGig } from '@/lib/stripe/get-payment-account-details-for-gig';
+import { holdGigAmount } from '@/lib/stripe/hold-gig-amount';
+
+async function updateAdjustedGig(
+  { gigId, newFinalRate, newFinalHours }: { gigId: string, newFinalRate: number, newFinalHours: number }
+) {
+  try {
+    const newFinalPrice = Math.round(newFinalRate * newFinalHours * 100);
+
+    await db.update(GigsTable)
+      .set({
+        finalRate: newFinalRate.toString(),
+        finalHours: newFinalHours.toString(),
+        finalAgreedPrice: newFinalPrice.toString(),
+        adjustedAt: new Date(),
+        updatedAt: new Date()
+      })
+      .where(eq(GigsTable.id, gigId));
+
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function handleGigAdjustment(
+  firebaseUid: string,
+  gigId: string,
+  newFinalRate: number,
+  newFinalHours: number,
+  currency: string,
+) {
+  if (!firebaseUid) {
+    return { error: 'User ID is required.', status: 400 }
+  }
+
+  const userRecord = await db.query.UsersTable.findFirst({
+    where: eq(UsersTable.firebaseUid, firebaseUid),
+  });
+
+  if (!userRecord) throw new Error('User not found');
+
+  const buyerStripeCustomerId = userRecord?.stripeCustomerId;
+
+  if (!buyerStripeCustomerId) {
+    throw new Error('User is not connected with stripe');
+  }
+
+  const { receiverAccountId, gig } = await getPaymentAccountDetailsForGig(gigId);
+
+  const currentFinalPrice = Number(gig.finalAgreedPrice) || Number(gig.totalAgreedPrice);
+  const newFinalPriceCents = Math.round(newFinalRate * newFinalHours * 100);
+
+  if (newFinalPriceCents <= currentFinalPrice) {
+    await updateAdjustedGig({ gigId, newFinalRate, newFinalHours });
+    return;
+  }
+
+  const differenceCents = newFinalPriceCents - currentFinalPrice;
+
+  holdGigAmount({
+    buyerStripeCustomerId,
+    destinationAccountId: receiverAccountId as string,
+    currency,
+    serviceAmountInCents: differenceCents,
+    description: `Adjustment for Gig ID: ${gigId}`,
+    gigPaymentInfo: {
+      gigId: gigId,
+      payerUserId: userRecord.id,
+      receiverUserId: gig?.workerUserId as string,
+    },
+    metadata: {
+      paymentType: 'ADJUSTMENT',
+    },
+  });
+
+  await updateAdjustedGig({ gigId, newFinalRate, newFinalHours });
+}
