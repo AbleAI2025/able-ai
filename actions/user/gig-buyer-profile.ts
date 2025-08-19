b19d58b0f7e896a97bbc8810a9a21d9970fbcb57
EricFerreyra <yoelferreya24@gmail.com>
2025-08-19 18:07:52 -0300

    fix: update storage rules to clarify upload permissions for buyer intro videos

--- actions/user/gig-buyer-profile.ts
+++ actions/user/gig-buyer-profile.ts
@@ -1,36 +1,42 @@
 "use server";
 
 import { db } from "@/lib/drizzle/db";
-import {
-    BuyerProfilesTable,
-    UsersTable,
-} from "@/lib/drizzle/schema";
+import { BuyerProfilesTable, UsersTable } from "@/lib/drizzle/schema";
 import { ERROR_CODES } from "@/lib/responses/errors";
 import { isUserAuthenticated } from "@/lib/user.server";
 import { eq } from "drizzle-orm";
 
 export const getGigBuyerProfile = async (
   token: string | undefined
-): Promise<{ success: true, profile: any }> => {
+): Promise<{ success: true; profile: any }> => {
   try {
-  if (!token) {
-    throw new Error("User ID is required to fetch buyer profile");
-  }
+    if (!token) {
+      throw new Error("User ID is required to fetch buyer profile");
+    }
 
-  const { uid } = await isUserAuthenticated(token);
-  if (!uid) throw ERROR_CODES.UNAUTHORIZED;
+    const { uid } = await isUserAuthenticated(token);
+    if (!uid) throw ERROR_CODES.UNAUTHORIZED;
 
-  const user = await db.query.UsersTable.findFirst({
-    where: eq(UsersTable.firebaseUid, uid),
-  });
+    const user = await db.query.UsersTable.findFirst({
+      where: eq(UsersTable.firebaseUid, uid),
+    });
 
-  if (!user) {
-    throw new Error("User not found");
-  }
-    
-      const buyerProfile = await db.query.BuyerProfilesTable.findFirst({
-        where: eq(BuyerProfilesTable.id, user.id),
-      });
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const buyerProfile = await db.query.BuyerProfilesTable.findFirst({
+      where: eq(BuyerProfilesTable.id, user.id),
+    });
+
+    const reviews = await db.query.ReviewsTable.findMany({
+      where: eq(BuyerProfilesTable.id, user.id),
+    });
+
+    const data = {
+      ...buyerProfile,
+      reviews
+    };
 
     if (!buyerProfile) throw "Getting worker profile error";
 
