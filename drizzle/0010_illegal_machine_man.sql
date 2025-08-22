6f9791fecd55822f0430ef8d5d7597da99aaaa4c
Jose Tovar <jtovar26455462@gmail.com>
2025-08-22 11:22:42 -0400

    chore: restore migration

--- drizzle/0010_illegal_machine_man.sql
+++ drizzle/0010_illegal_machine_man.sql
@@ -2,4 +2,4 @@ ALTER TABLE "gigs" ADD COLUMN "final_rate" numeric(10, 2);--> statement-breakpoi
 ALTER TABLE "gigs" ADD COLUMN "final_hours" numeric(5, 2);--> statement-breakpoint
 ALTER TABLE "gigs" ADD COLUMN "final_agreed_price" numeric(10, 2);--> statement-breakpoint
 ALTER TABLE "gigs" ADD COLUMN "adjustment_notes" text;--> statement-breakpoint
-ALTER TABLE "gigs" ADD COLUMN "adjusted_at" timestamp with time zone;
+ALTER TABLE "gigs" ADD COLUMN "adjusted_at" timestamp with time zone;--> statement-breakpoint
