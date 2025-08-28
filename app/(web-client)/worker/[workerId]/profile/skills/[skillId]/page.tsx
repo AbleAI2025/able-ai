7ca52b69917de01446af3741969f95598d09ccb2
yodit93 <yoditabebe9391@gmail.com>
2025-08-28 19:30:52 +0300

    feat: add onBackClick prop

--- app/(web-client)/worker/[workerId]/profile/skills/[skillId]/page.tsx
+++ app/(web-client)/worker/[workerId]/profile/skills/[skillId]/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import styles from './SkillSpecificPage.module.css';
 import SkillSplashScreen from '@/app/components/profile/SkillSplashScreen';
 import CloseButton from '@/app/components/profile/CloseButton';
 import HireButton from '@/app/components/profile/HireButton';
 import { getSkillDetailsWorker } from '@/actions/user/gig-worker-profile';
-import { Star as DefaultBadgeIcon } from "lucide-react";
+import { Star as DefaultBadgeIcon, Router } from "lucide-react";
 import { SkillProfile } from '@/app/(web-client)/user/[userId]/worker/profile/skills/[skillId]/schemas/skillProfile';
 import { mockSkillProfile } from '@/app/(web-client)/user/[userId]/worker/profile/skills/[skillId]/mockSkillProfile';
 import Loader from '@/app/components/shared/Loader';
@@ -19,6 +19,7 @@ export default function PublicSkillProfilePage() {
   const params = useParams();
   const skillId = params?.skillId as string;
   const [profile, setProfile] = useState<SkillProfile | null>(null);
+  const router = useRouter();
 
   const isViewQA = false;
 
@@ -63,7 +64,13 @@ export default function PublicSkillProfilePage() {
   return (
     <div className={styles.skillPageContainer}>
       <CloseButton />
-      <SkillSplashScreen profile={profile} skillId={skillId} fetchSkillData={fetchSkillData} isSelfView={false} />
+      <SkillSplashScreen 
+        profile={profile} 
+        skillId={skillId} 
+        fetchSkillData={fetchSkillData} 
+        isSelfView={false} 
+        onBackClick={() => router.back()}
+      />
       <HireButton workerId={skillId} workerName={profile?.name} />
     </div> 
   );
