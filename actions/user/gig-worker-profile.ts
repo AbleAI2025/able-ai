b1a6c5e23943bf2866f6f56868998bb5793c1836
Anthony Rosales <israellaguan@gmail.com>
2025-10-07 02:50:58 -0500

    fix: improve user profile handling and redirection logic

--- actions/user/gig-worker-profile.ts
+++ actions/user/gig-worker-profile.ts
@@ -77,9 +77,9 @@ export const getGigWorkerProfile = async (
         user?: { fullName: string; rtwStatus: string | null };
       })
     | undefined
-): Promise<{ success: true; data: PublicWorkerProfile }> => {
+): Promise<{ success: true; data?: PublicWorkerProfile }> => {
   try {
-    if (!workerProfile) throw "Getting worker profile error";
+    if (!workerProfile) return { success: true};
 
     const skills = await db.query.SkillsTable.findMany({
       where: eq(SkillsTable.workerProfileId, workerProfile.id),
